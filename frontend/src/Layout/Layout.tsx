494fb7809b3d2476b2e75d0ddeb6718729429ef5
PanMingwei <panmingwei123@gmail.com>
2023-05-26 19:52:57 +0800

    navbar

--- frontend/src/Layout/Layout.tsx
+++ frontend/src/Layout/Layout.tsx
@@ -14,9 +14,6 @@ export const Layout = () => {
     }, []);
 
 
-
-
-
     return (
         <div className="App">
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from './Layout';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // Load ENV variables
+// dotEnvConfig({
+// 	path:
+// 		process.env.NODE_ENV === 'production'
+// 			? '.env.production'
+// 			: '.env.development',
+// });
+
+
+ReactDOM.render(
+	<BrowserRouter>
+
+		
+		<Routes>
+			<Route path="/" element={<Layout />} />
+		
+		</Routes>
+	</BrowserRouter>,
+	document.getElementById('root'));
+
+function dotEnvConfig(arg0: { path: string; }) {
+  throw new Error('Function not implemented.');
+}
