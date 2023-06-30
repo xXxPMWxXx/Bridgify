8b9e07e72c510edb62b5873545defed49ae5218d
xXxPMWxXx <panmingwei123@gmail.com>
2023-06-30 14:20:27 +0800

    add model file

---
+++ backend/src/models/elderly.ts
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const uniqueValidator = require("mongoose-unique-validator");
+
+
+// add this to advice cannot redeclare block-scoped variable error
+export{}
+
