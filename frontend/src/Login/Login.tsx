d94bd6d14fc9192513123c9305393e9aea2ddf4d
petrinawjt <petrinawong1909@gmail.com>
2023-07-07 19:39:26 +0800

    Log In Page

--- frontend/src/Login/Login.tsx
+++ frontend/src/Login/Login.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import backgroundImage from './images/logInBackground.jpeg';
+import icon from './images/icon.png';
+import {Helmet} from 'react-helmet';
+import './logIn.css';
 import {
   MDBBtn,
   MDBContainer,
@@ -11,37 +15,52 @@ from 'mdb-react-ui-kit';
   
 
 
-export const Login = () => {
+export const LogIn = () => {
 
     return (
         <MDBContainer fluid>
             <MDBRow>
-
                 <MDBCol sm='6'>
 
-                    <div className='d-flex flex-row ps-5 pt-5'>
-                        <MDBIcon fas icon="crow fa-3x me-3" style={{ color: '#709085' }} />
-                        <span className="h1 fw-bold mb-0">Logo</span>
+                    <div className='background'>
+                        <Helmet>
+                            <style>
+                                {'body { background-color: #FEF9F9; }'}
+                            </style>
+                        </Helmet>
                     </div>
 
-                    <div className='d-flex flex-column justify-content-center h-custom-2 w-75 pt-4'>
+                    <div className='d-flex flex-row ps-5 pt-5'>
+                        <img src={icon} alt="Logo" width={35} height={35}/>
+                        <span className="h1 fw-bold mb-0" style={{color:'black', fontSize:18, textAlign:'center', marginTop:7, marginLeft:7}}>Bridgify</span>
+                    </div>
 
-                        <h3 className="fw-normal mb-3 ps-5 pb-3" style={{ letterSpacing: '1px' }}>Log in</h3>
 
-                        <MDBInput wrapperClass='mb-4 mx-5 w-100' label='Email address' id='formControlLg' type='email' size="lg" />
-                        <MDBInput wrapperClass='mb-4 mx-5 w-100' label='Password' id='formControlLg' type='password' size="lg" />
+                    <div className='logIn'>
 
-                        <MDBBtn className="mb-4 px-5 mx-5 w-100" color='info' size='lg'>Login</MDBBtn>
-                        <p className="small mb-5 pb-lg-3 ms-5"><a className="text-muted" href="#!">Forgot password?</a></p>
-                        <p className='ms-5'>Don't have an account? <a href="#!" className="link-info">Register here</a></p>
+                        <h3 style={{color:'black', fontSize:60, fontWeight:700, textAlign:'center', marginTop:80, letterSpacing:0}}>Hi there!</h3>
+                        <h4 style={{textAlign:'center', color:'black', fontSize:20, marginTop:-15, fontWeight:400}}>Welcome to Bridgify</h4>
+
+                        <MDBInput wrapperClass='mb-3 mx-5 w-75' label='Your email' id='formControlLg' type='email' size='lg' style={{marginTop: 40}}/>
+                        <MDBInput wrapperClass='mb-1 mx-5 w-75' label='Password' id='formControlLg' type='password' size="lg" />
+
+
+                        <p className="small mb-4 pb-lg-3 ms-5"><a className="text-muted" href="#!">Forgot password?</a></p>
+                        <MDBBtn className="mb-4 px-5 mx-5 w-75" size='lg' rounded color='dark'>Login</MDBBtn>
+                        <p className='ms-5'>Don't have an account? <a href="#!" className="link-info">Sign Up</a></p>
+
 
                     </div>
 
                 </MDBCol>
 
                 <MDBCol sm='6' className='d-none d-sm-block px-0'>
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img3.webp"
-                        alt="Login image" className="w-100" style={{ objectFit: 'cover', objectPosition: 'left' }} />
+                    <div>
+                        <img src={backgroundImage}
+                        alt="Login image" className="w-100" style={{ objectFit: 'cover', objectPosition: 'left', width: "50%", height:"100%", justifyContent: "center", }}  />
+                        <p style={{color:'white' ,position:'absolute', left:1300, top:58, fontSize:13, fontWeight:500}}>SIGN UP</p>
+                        <MDBBtn rounded outline color='light'style={{position:'absolute', left:1370, top:50}}>Admin</MDBBtn>
+                    </div>
                 </MDBCol>
 
             </MDBRow>
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,7 +8,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from './Layout';
 import { Home } from './Home';
-import { Login } from './Login';
+import { LogIn } from './Login';
 
 //this have issue need find out why
 // // Load ENV variables
@@ -25,7 +25,7 @@ ReactDOM.render(
 		<Layout />
 		<div className='mt-n1'></div>
 		<Routes>
-			<Route path="/Login" element={<Login />} />
+			<Route path="/Login" element={<LogIn />} />
 			<Route path="/Home" element={<Home />} />
 		</Routes>
 	</BrowserRouter>,
