cdfb01983c1879e13cbc1d2e898f81451ac12673
georgiaxng <georgiaxng@gmail.com>
2023-07-12 05:57:27 +0800

    Record Display & Get Methods

--- backend/src/services/record/controller.ts
+++ backend/src/services/record/controller.ts
@@ -50,7 +50,7 @@ export const test = async (req: any, res: any, next: NextFunction) => {
 
 };
 
-// to create a new post
+// to create a new record
 export const create = async (req: any, res: any, next: NextFunction) => {
 
   // 1. get token from req
@@ -64,8 +64,8 @@ export const create = async (req: any, res: any, next: NextFunction) => {
         const data = req.body;
         const file = req.files.doc;
 
-        //only accepts one file, assumes document_no is unique
-        const fileName = `${data.name}_${data.document_no}` +path.extname(file.name);
+        //only accepts one file
+        const fileName = `${data.elderly_name}_${data.name}_${data.document_no}` + path.extname(file.name);
         file.mv(baseDir + `/records/${fileName}`);
 
         //create the DB object
@@ -78,7 +78,7 @@ export const create = async (req: any, res: any, next: NextFunction) => {
           "document_path": fileName
         });
 
-         // add a new record to mongodb
+        // add a new record to mongodb
         newRecord
           .save()
           .then((response: any) => {
@@ -89,8 +89,8 @@ export const create = async (req: any, res: any, next: NextFunction) => {
           .catch((error: any) => {
             return next(error);
           });
-      
-      
+
+
       } else if (err) {
         res.status(401).json({ error: "You must have a valid token" });
       }
@@ -100,3 +100,103 @@ export const create = async (req: any, res: any, next: NextFunction) => {
     }
   });
 };
+
+// to delete a new post
+// export const deleteRecord = async (req: any, res: any, next: NextFunction) => {
+
+//   // 1. get token from req
+//   const token =
+//     req.headers.authorization && req.headers.authorization.split(" ")[1];
+
+//   // 2. verify token with secret key
+//   jwt.verify(token, jwt_secret, async (err: any, decoded: any) => {
+//     try {
+//       if (decoded) {
+//         const data = req.body;
+
+//         const record = await RecordModel.findOne({ document_no: id });
+//       }else if (err) {
+//   res.status(401).json({ error: "You must have a valid token" });
+// }
+//     } catch (error) {
+//       return res.status(400).json({ message: "Please make sure the input file is valid type", error: String(error) });
+//     }
+//   });
+// };
+
+// to display document
+export const display = async (req: any, res: any, next: NextFunction) => {
+
+  // 1. get token from req
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
+
+  // 2. verify token with secret key
+  jwt.verify(token, jwt_secret, async (err: any, decoded: any) => {
+    try {
+      if (decoded) {
+        const data = req.body;
+        const fileName = data.fileName;
+        res.sendFile(baseDir + `/records/${fileName}`);
+      } else if (err) {
+        res.status(401).json({ error: "You must have a valid token" });
+      }
+    } catch (error) {
+      return res.status(400).json({ message: "Please make sure the input file is valid type", error: String(error) });
+    }
+  });
+};
+
+// to get all records for one specific elderly for user side
+export const getSelected = async (req: any, res: any, next: NextFunction) => {
+
+  // 1. get token from req
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
+
+  // 2. verify token with secret key
+  jwt.verify(token, jwt_secret, async (err: any, decoded: any) => {
+    try {
+      if (decoded) {
+        const data = req.body;
+        // console.log(data);
+        const records = await RecordModel.find({ elderlyID: data.elderlyID });
+        res.status(200).json({ records });
+
+      } else if (err) {
+        res.status(401).json({ error: "You must have a valid token" });
+      }
+    } catch (error) {
+      return res.status(400).json({ message: "Please make sure the input file is valid type", error: String(error) });
+    }
+  });
+};
+
+
+
+// to get all records for admin
+export const getAll = async (req: any, res: any, next: NextFunction) => {
+
+  // 1. get token from req
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
+
+  // 2. verify token with secret key
+  jwt.verify(token, jwt_secret, async (err: any, decoded: any) => {
+    try {
+      if (decoded) {
+        const allRecords = await RecordModel.find({});
+        res.status(200).json(allRecords);
+
+      } else if (err) {
+        res.status(401).json({ error: "You must have a valid token" });
+      }
+    } catch (error) {
+      return res.status(400).json({ message: "Please make sure the input file is valid type", error: String(error) });
+    }
+  });
+};
+
+
+
+
--- backend/src/services/record/routes.ts
+++ backend/src/services/record/routes.ts
@@ -11,4 +11,16 @@ recordRouter.route('/test').get(controller.test);
 /** POST /api/record/create */
 recordRouter.route('/create').post(controller.create);
 
+/** GET /api/record/display */
+recordRouter.route('/display').get(controller.display);
+
+/** POST /api/record/delete */
+// recordRouter.route('/delete').post(controller.deleteRecord);
+
+/** GET /api/record/getSelected */
+recordRouter.route('/getSelected').get(controller.getSelected);
+
+/** GET /api/record/getAll for the admin*/
+recordRouter.route('/getAll').get(controller.getAll);
+
 
