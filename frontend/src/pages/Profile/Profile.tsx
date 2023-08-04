ea07d8b67d4bb49a90eeb1a96e71034f361c9c1c
georgiaxng <73217466+georgiaxng@users.noreply.github.com>
2023-08-05 05:02:53 +0800

    implemented link elderly

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
 import { ResponsiveAppBar } from '../../Navbar';
 import { styled } from '@mui/material/styles';
-import { Alert, Avatar, Box, Button, FormControl, FormControlLabel, Grid, IconButton, Radio, RadioGroup, Snackbar, TextField, Typography } from '@mui/material';
+import { Alert, Avatar, Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, FormControlLabel, Grid, IconButton, Radio, RadioGroup, Snackbar, TextField, Typography } from '@mui/material';
 import Table from '@mui/joy/Table';
 import { Sheet } from '@mui/joy';
 import LinkOffIcon from '@mui/icons-material/LinkOff';
@@ -30,6 +30,10 @@ export const Profile = () => {
     const email = window.localStorage.getItem('email')
 
     const profileImageSrc = `${process.env.REACT_APP_BACKEND_IMAGES_URL}/user_profile/${profileImage}`;
+
+    //dialog
+    const [open, setOpen] = React.useState(false);
+
 
     const [linkedElderlyData, setLinkedElderlyData] = React.useState<any[]>([]);
 
@@ -78,10 +82,11 @@ export const Profile = () => {
 
     const handleSubmit = (event: any) => {
         event.preventDefault();
+        console.log("Handle submit clicked")
         var APIMethod = 'linkElderly';
-        if (mode != 'Add') {
-            APIMethod = 'remove-linkElderly';
-        }
+        // if (mode != 'Add') {
+        //     APIMethod = 'remove-linkElderly';
+        // }
         // //calling backend API
         fetch(`${process.env.REACT_APP_BACKEND_PRODUCTION_URL}/user/${APIMethod}`, {
             headers: {
@@ -109,7 +114,10 @@ export const Profile = () => {
                     //show alert msg
                     setOpenSnackbar(true);
                     setAlertType('success');
-                    setAlertMsg(apiResponse['message']);
+                    setAlertMsg(apiResponse['message']);        
+                    
+                    handleClose();
+
                 }
 
             })
@@ -164,6 +172,15 @@ export const Profile = () => {
         setOpenSnackbar(false);
     };
 
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     useEffect(() => {
         elderlyFetcher()
     }, [linkedElderly])
@@ -174,172 +191,155 @@ export const Profile = () => {
                     <Navigate to="/Login" /> : <Navigate to="/Profile" />
             }
             < ResponsiveAppBar />
-            <Grid
-                container
-                spacing={1}
-                alignItems="center"
-                justifyContent="center"
-                sx={{ minHeight: '24vh', bgcolor: '#30685E' }}
-            >
-                <Grid
-                    container
-                    spacing={1}
-                    alignItems="center"
-                    sx={{ width: "auto" }}
-
-                >
+            <Grid container sx={{ justifyContent: "center", marginTop: "5%" }}>
+                <Grid item sx={{
+                    border: "2px solid black", justifyContent: "center", alignItems: "center", p: 2,
+                    width: "auto", height: 500, marginRight: 5, bgcolor: "#30685E", borderRadius: 5
+                }}>
                     <Avatar
                         alt="User Photo"
                         src={profileImageSrc}
-                        sx={{ width: 100, height: 100, marginRight: 3 }}
+                        sx={{ width: 200, height: 200, margin: "auto", marginTop: 5 }}
                     />
-                    <Typography variant="h5" component="h1" sx={{ color: 'white' }} >
+                    <Typography variant="h5" sx={{ color: "white", marginTop: "10%", paddingLeft: "10%", width: "auto" }} >
+                        <b>Profile:</b><br />
                         User: {userName} <br />
                         Email: {email}<br />
                         Account Role: {accRole}<br />
                     </Typography>
                 </Grid>
-                {/* <Avatar
-                    alt="User Photo"
-                    src={profileImageSrc}
-                    sx={{ width: 70, height: 70 }}
-                />
-                <Box >
-                    <p />
-                    <Typography variant="h4" component="h1" sx={{ color: '#03a9f4' }} >
-                        {userName}
-                    </Typography>
-                    <p />
-                    <Typography variant="h6" sx={{ m: 1, color: '#00bcd4' }}>
-                        Email : {email}
-                        <br />
-                        Account Role : {accRole}
-                        <br />
-                        Linked Elderly : {linkedElderly}
-                    </Typography>
-                </Box> */}
-            </Grid>
-            <Box >
+                <Grid item>
 
-                <Box sx={{
-                    width: 500, margin: "auto",
-                    alignContent: "center",
-                    marginTop: 2
-                }}><Sheet>
-                        {/* <Typography variant="h4" alignContent="center" component="h1" sx={{ m: 2, display:"inline-block"  }} >
-                        Linked Elderly
-                    </Typography > */}
-                        <Button
-                            startIcon={<LinkIcon />}
-                            disabled={false}
-                            size="small"
+                    <Box sx={{
+                        width: 500, margin: "auto",
+                        alignContent: "center",
+                    }}><Sheet>
+                            <Button
+                                startIcon={<LinkIcon />}
+                                disabled={false}
+                                onClick={handleClickOpen}
+                                size="small"
+                                variant="outlined"
+                                sx={{
+                                    borderWidth: "2px",
+                                    borderColor: "#30685e",
+                                    backgroundColor: "white",
+                                    color: "#30685e",
+                                    ":hover": {
+                                        bgcolor: "#224942",
+                                        color: "white"
+                                    }, marginBottom: 2,
+                                    float: "right"
+                                }}> Link Elderly</Button>
+                        </Sheet>
+                        <Sheet
+                            className="HealthTableContainer"
                             variant="outlined"
                             sx={{
-                                borderWidth: "2px",
-                                borderColor: "#30685e",
-                                backgroundColor: "white",
-                                color: "#30685e",
-                                ":hover": {
-                                    bgcolor: "#224942",
-                                    color: "white"
-                                }, marginBottom: 2, clear: "left",
-                                float: "right"
-                            }}> Link Elderly</Button>
-                    </Sheet>
-                    <Sheet
-                        className="HealthTableContainer"
-                        variant="outlined"
-                        sx={{
-                            width: 500,
-                            borderRadius: 'md',
-                            flex: 1,
-                            overflow: 'auto',
-                        }}
-                    >
-                        {(linkedElderlyData.length != 0) ?
-                            <Table
-                                aria-labelledby="tableTitle"
-                                stickyHeader
-                                hoverRow
-                                sx={{
-                                    '--TableCell-headBackground': (theme: { vars: { palette: { background: { level1: any; }; }; }; }) =>
-                                        theme.vars.palette.background.level1,
-                                    '--Table-headerUnderlineThickness': '1px',
-                                    '--TableRow-hoverBackground': (theme: { vars: { palette: { background: { level1: any; }; }; }; }) =>
-                                        theme.vars.palette.background.level1,
-                                }}
-                            >
-                                <Box sx={{ borderBottom: 1, borderColor: 'divider' }} />
-                                <thead >
-                                    <tr style={{}}>
+                                width: 500,
+                                borderRadius: 'md',
+                                flex: 1,
+                                overflow: 'auto',
+                            }}
+                        >
+                            {(linkedElderlyData.length != 0) ?
+                                <Table
+                                    aria-labelledby="tableTitle"
+                                    stickyHeader
+                                    hoverRow
+                                    sx={{
+                                        '--TableCell-headBackground': (theme: { vars: { palette: { background: { level1: any; }; }; }; }) =>
+                                            theme.vars.palette.background.level1,
+                                        '--Table-headerUnderlineThickness': '1px',
+                                        '--TableRow-hoverBackground': (theme: { vars: { palette: { background: { level1: any; }; }; }; }) =>
+                                            theme.vars.palette.background.level1,
+                                    }}
+                                >
+                                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }} />
+                                    <thead >
+                                        <tr style={{}}>
 
-                                        <th style={{ padding: 12, backgroundColor: "white", width: "20%" }}>ID</th>
-                                        <th style={{ padding: 12, backgroundColor: "white", width: "40%" }}>Name</th>
-                                        <th style={{ padding: 12, backgroundColor: "white", width: "25%" }}>Date of Birth</th>
-                                        <th style={{ padding: 12, backgroundColor: "white", width: "15%" }}></th>
+                                            <th style={{ padding: 12, backgroundColor: "white", width: "20%" }}>ID</th>
+                                            <th style={{ padding: 12, backgroundColor: "white", width: "40%" }}>Name</th>
+                                            <th style={{ padding: 12, backgroundColor: "white", width: "25%" }}>Date of Birth</th>
+                                            <th style={{ padding: 12, backgroundColor: "white", width: "15%" }}></th>
 
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {linkedElderlyData.map((row) => (
-                                        <tr key={row.id} >
-                                            <td style={{ padding: 12, width: "5%" }}>{row.id}</td>
-                                            <td >
-                                                <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
-                                                    <Avatar alt={row.elderlyName} src={`${imageBASEURL}/${row.photo}`} />
-                                                    <Typography sx={{ font: "inherit" }}>
-                                                        {row.name}
-                                                    </Typography>
-                                                </Box>
-                                            </td>
-                                            <td style={{ padding: 12 }}>{row.DOB}</td>
-
-                                            <td style={{
-                                                width: "5%",
-
-
-                                            }}>
-                                                <IconButton aria-label="unlink" onClick={() => handleRemove(row.id)}>
-                                                    <LinkOffIcon /></IconButton>
-                                            </td>
                                         </tr>
-                                    ))}
-                                </tbody>
+                                    </thead>
+                                    <tbody>
+                                        {linkedElderlyData.map((row) => (
+                                            <tr key={row.id} >
+                                                <td style={{ padding: 12, width: "5%" }}>{row.id}</td>
+                                                <td >
+                                                    <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+                                                        <Avatar alt={row.elderlyName} src={`${imageBASEURL}/${row.photo}`} />
+                                                        <Typography sx={{ font: "inherit" }}>
+                                                            {row.name}
+                                                        </Typography>
+                                                    </Box>
+                                                </td>
+                                                <td style={{ padding: 12 }}>{row.DOB}</td>
 
-                            </Table> : null}
+                                                <td style={{
+                                                    width: "5%",
 
-                    </Sheet>
+
+                                                }}>
+                                                    <IconButton aria-label="unlink" onClick={() => handleRemove(row.id)}>
+                                                        <LinkOffIcon /></IconButton>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+
+                                </Table> : null}
+
+                        </Sheet>
+                    </Box></Grid>
+
+            </Grid>
+                <Dialog open={open} onClose={handleClose} >
+                    <DialogTitle>Link Elderly</DialogTitle>
+                    <DialogContent>
+               
+
+                        <TextField onChange={handleElderlyID} required label="Elderly ID (Last 4 digit of NRIC)" variant="outlined" sx={{ width: 300, marginTop:1}} />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button type="submit" onClick={handleSubmit}>Link</Button>
+                    </DialogActions>
+                </Dialog>
+            {/* <form onSubmit={handleSubmit}>
+                <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                    <RadioGroup
+                        row
+                        defaultValue="Add"
+                        onChange={handleMode}
+                    >
+                        <FormControlLabel value="Add" control={<Radio />} label="Add" />
+                        <FormControlLabel value="Remove" control={<Radio />} label="Remove" />
+
+                    </RadioGroup>
+                    <TextField onChange={handleElderlyID} margin="normal" required autoFocus label="Elderly ID(last 4 digit of NRIC)" sx={{ width: 400 }} />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        sx={{ width: 300, m: 2 }}
+                    >
+                        Submit
+                    </Button>
                 </Box>
-                {/* {rows.length > 0 ? <Typography></Typography> : <Box textAlign={'center'} sx={{ p: 5 }} fontSize={"24px"}>No Records Found...</Box>} */}
 
-                <form onSubmit={handleSubmit}>
-                    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
-                        <RadioGroup
-                            row
-                            defaultValue="Add"
-                            onChange={handleMode}
-                        >
-                            <FormControlLabel value="Add" control={<Radio />} label="Add" />
-                            <FormControlLabel value="Remove" control={<Radio />} label="Remove" />
+             
+            </form>    */}
 
-                        </RadioGroup>
-                        <TextField onChange={handleElderlyID} margin="normal" required autoFocus label="Elderly ID(last 4 digit of NRIC)" sx={{ width: 400 }} />
-                        <Button
-                            type="submit"
-                            variant="contained"
-                            sx={{ width: 300, m: 2 }}
-                        >
-                            Submit
-                        </Button>
-                    </Box>
 
-                    <Snackbar open={openSnackbar} autoHideDuration={3000} onClose={handleSnackbarClose}>
-                        <Alert onClose={handleSnackbarClose} severity={alertType} sx={{ width: '100%' }}>
-                            {alertMsg}
-                        </Alert>
-                    </Snackbar>
-                </form>
-            </Box>
-        </div>
+            <Snackbar open={openSnackbar} autoHideDuration={3000} onClose={handleSnackbarClose}>
+                <Alert onClose={handleSnackbarClose} severity={alertType} sx={{ width: '100%' }}>
+                    {alertMsg}
+                </Alert>
+            </Snackbar>
+        </div >
     )
 }
